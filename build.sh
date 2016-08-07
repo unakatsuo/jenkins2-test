bf99f79e444b4c612fe58864cfeee31e3854f099
Masahiro Fujiwara <m-fujiwara@axsh.net>
2016-08-07 20:39:32 +0900

    change

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -ex -o pipefail
 
 img_tag="openvnet/${BRANCH_NAME}"
 docker build -t "${img_tag}" .
-cid=$(docker run "${img_tag}")
+cid=$(docker run -d "${img_tag}")
 (rm -rf openvnet; git clone --depth=1 https://github.com/axsh/openvnet.git)
-docker cp openvnet ${cid}:openvnet
+(
+    docker cp openvnet ${cid}:openvnet
+) || :
+docker rm $cid
