13ab8fac4b1b49f7144b3049064b02b881a3bd22
Masahiro Fujiwara <m-fujiwara@axsh.net>
2016-08-07 21:03:32 +0900

    fix

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ cid=$(docker run -d "${img_tag}")
 (rm -rf openvnet; git clone --depth=1 https://github.com/axsh/openvnet.git)
 (
     docker cp openvnet "${cid}:/var/tmp"
-    docker exec $cid -t /bin/bash -c "cd openvnet; ./deployment/packagebuild/build_packages_vnet.sh"
+    docker exec -t $cid /bin/bash -c "cd openvnet; ./deployment/packagebuild/build_packages_vnet.sh"
 ) || :
 docker rm -f $cid
