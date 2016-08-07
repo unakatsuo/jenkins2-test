d54569381c6ce010b7646208989b442f17510c49
Masahiro Fujiwara <m-fujiwara@axsh.net>
2016-08-07 20:34:47 +0900

    change

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,4 @@
 FROM centos:7
 RUN yum install -y yum-utils createrepo rpm-build
+WORKDIR /var/tmp
+ENTRYPOINT ["/sbin/init"]
--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -ex -o pipefail
 img_tag="openvnet/${BRANCH_NAME}"
 docker build -t "${img_tag}" .
 cid=$(docker run "${img_tag}")
-(rm -rf ovn; git clone --depth=1 https://github.com/axsh/openvnet.git ovn)
-#docker cp ovn 
+(rm -rf openvnet; git clone --depth=1 https://github.com/axsh/openvnet.git)
+docker cp openvnet ${cid}:openvnet
